b3bef9b612e81d17ddb546b06f94ae71078e7ce4
Nick Stracke <nick.stracke@web.de>
2021-08-10 14:25:40 +0200

    add pass to if

--- utils_sc.py
+++ utils_sc.py
@@ -19,8 +19,10 @@ def draw_data(metadf, imb_ratio, strat_dims, y='dx'):
   main_class_count = df[['image_id', y]].groupby(y).agg('count').sort_values(by='image_id', ascending=False)['image_id'][0]
   if main_class_count >= imb_ratio * (total_count - main_class_count):
     #TODO: take all rows from other classes and sample imb_ratio * (total_count - main_class_count) rows from main class
+    pass
   else:
     #TODO: take all rows from main class and sample floor(main_class_count / imb_ratio) rows from other classes
+    pass
   return df
 
 def load_metadf():
