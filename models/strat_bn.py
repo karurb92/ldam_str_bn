a6be3a516b60239f50316eb6f7a4bf9e8778f81f
Nick Stracke <nick.stracke@web.de>
2021-08-25 16:44:06 +0200

    start with batch norm layer

--- models/strat_bn.py
+++ models/strat_bn.py
@@ -1,53 +1,104 @@
 import tensorflow as tf
 
-
-# =====================
-# Option 1
-# =====================
-
-# https://www.tensorflow.org/guide/intro_to_modules#waiting_to_create_variables
-class FlexibleDenseModule(tf.Module):
-  # Note: No need for `in_features`
-  def __init__(self, out_features, name=None):
-    super().__init__(name=name)
-    self.is_built = False
-    self.out_features = out_features
-
-  def __call__(self, x):
-    # Create variables on first call.
-    if not self.is_built:
-      self.w = tf.Variable(
-        tf.random.normal([x.shape[-1], self.out_features]), name='w')
-      self.b = tf.Variable(tf.zeros([self.out_features]), name='b')
-      self.is_built = True
-
-    y = tf.matmul(x, self.w) + self.b
-    return tf.nn.relu(y)
-
-
-# =====================
-# Option 2
-# =====================
-
 # Source:
 # https://www.tensorflow.org/tutorials/customization/custom_layers#implementing_custom_layers
-class MyDenseLayer(tf.keras.layers.Layer):
+
+
+class StratBN(tf.keras.layers.Layer):
 
     # __init__ , where you can do all input-independent initialization
-    def __init__(self, num_outputs):
-        super(MyDenseLayer, self).__init__()
-        self.num_outputs = num_outputs
+    def __init__(self):
+        super(StratBN, self).__init__()
+
+        '''
+        During training (i.e. when using `fit()` or when calling the layer/model
+        with the argument `training=True`), the layer normalizes its output using
+        the mean and standard deviation of the current batch of inputs. That is to
+        say, for each channel being normalized, the layer returns
+        `gamma * (batch - mean(batch)) / sqrt(var(batch) + epsilon) + beta`, where:
+
+        - `epsilon` is small constant (configurable as part of the constructor
+        arguments)
+        - `gamma` is a learned scaling factor (initialized as 1)
+        - `beta` is a learned offset factor (initialized as 0)
+        '''
+
+        self.epsilon = 1e-3
+        self.beta = 0
+        self.gamma = 1
+
+        '''
+        During inference (i.e. when using `evaluate()` or `predict()`) or when
+        calling the layer/model with the argument `training=False` (which is the
+        default), the layer normalizes its output using a moving average of the
+        mean and standard deviation of the batches it has seen during training. That
+        is to say, it returns
+        `gamma * (batch - self.moving_mean) / sqrt(self.moving_var + epsilon) + beta`.
+
+        `self.moving_mean` and `self.moving_var` are non-trainable variables that
+        are updated each time the layer in called in training mode, as such:
+
+        - `moving_mean = moving_mean * momentum + mean(batch) * (1 - momentum)`
+        - `moving_var = moving_var * momentum + var(batch) * (1 - momentum)`
+
+        As such, the layer will only normalize its inputs during inference
+        *after having been trained on data that has similar statistics as the
+        inference data*.
+        '''
+
+        self.moving_mean = 0
+        self.moving_variance = 1
+        self.momentum = 0.99
+
+    def _calculate_mean_and_var(self, inputs, reduction_axes, keep_dims):
+         return tf.nn.moments(inputs, reduction_axes, keepdims=keep_dims)
+
 
     # build, where you know the shapes of the input tensors and can do the rest of the initialization
+
     def build(self, input_shape):
-        self.kernel = self.add_weight("kernel",
-                                      shape=[int(input_shape[-1]),
-                                             self.num_outputs])
+        input_shape = tf.TensorShape(input_shape)
+        if not input_shape.ndims:
+            raise ValueError(
+                f'Input has undefined rank. Received: input_shape={input_shape}.')
+        ndims = len(input_shape)
+
+
+    
+        axis_to_dim = {x: input_shape.dims[x].value for x in self.axis}
+        for x in axis_to_dim:
+          if axis_to_dim[x] is None:
+            raise ValueError('Input has undefined `axis` dimension. Received input '
+                            'with shape %s. Axis value: %s' %
+                            (tuple(input_shape), self.axis))
+
+        # Single axis batch norm (most common/default use-case)
+        param_shape = (list(axis_to_dim.values())[0],)
+
+
+        self.gamma = self.add_weight(
+            name='gamma',
+            shape=param_shape,
+            dtype=self._param_dtype,
+            initializer=self.gamma_initializer,
+            regularizer=self.gamma_regularizer,
+            constraint=self.gamma_constraint,
+            trainable=True,
+            experimental_autocast=False)
+
+        self.beta = self.add_weight(
+            name='beta',
+            shape=param_shape,
+            dtype=self._param_dtype,
+            initializer=self.beta_initializer,
+            regularizer=self.beta_regularizer,
+            constraint=self.beta_constraint,
+            trainable=True,
+            experimental_autocast=False)
+
+        
 
     # call, where you do the forward computation
-    def call(self, inputs):
-        return tf.matmul(inputs, self.kernel)
 
-# create
-layer = MyDenseLayer(10)
-_ = layer(tf.zeros([10, 5])) # Calling the layer `.builds` it.
+    def call(self, inputs, training=None):
+        training = self._get_training_value(training)
---
+++ test.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+# import numpy as np
+
+# input_shape = tf.TensorShape([1, 2])
+print(1)
