f3874048a0844f27d14c9b4768825a44531c917c
Nick Stracke <nick.stracke@web.de>
2021-08-31 14:18:41 +0200

    start with training loop

--- train_sc.py
+++ train_sc.py
@@ -17,22 +17,31 @@ from losses import *
 # parse args here
 #imb_ratio, strat_dims, data_path, train_split, batch_size
 
+
 def main():
 
-    ### before we feed all imgs here, we should earlier decide on some constant test set
-    ### assess on imbalance or balance?
+    # before we feed all imgs here, we should earlier decide on some constant test set
+    # assess on imbalance or balance?
+    imb_ratio = 10
+    strat_dims = ['age_mapped']
+    train_split = 0.8
+    batch_size = 2
 
-    metadf = load_metadf(data_path)
-    data_train, data_val, labels, strat_classes_num = draw_data(metadf, imb_ratio, strat_dims, train_split)
-    
+    metadf = load_metadf(project_path)
+    data_train, data_val, labels, strat_classes_num = draw_data(
+        metadf, imb_ratio, strat_dims, train_split)
+
+    print(strat_classes_num)
+
     params_generator = {'dim': (450, 600, 3),
-          'batch_size': batch_size,
-          'n_classes': 7,
-          'shuffle': True}
+                        'batch_size': batch_size,
+                        'n_classes': 7,
+                        'shuffle': True}
 
-    training_generator = DataGenerator(data_train, labels, strat_classes_num, data_path, **params_generator)
-    validation_generator = DataGenerator(data_val, labels, strat_classes_num, data_path, **params_generator)
-
+    training_generator = DataGenerator(
+        data_train, labels, strat_classes_num, imgs_path, **params_generator)
+    validation_generator = DataGenerator(
+        data_val, labels, strat_classes_num, imgs_path, **params_generator)
 
     # callbacks = [
     #     # Write TensorBoard logs to `./logs` directory
@@ -40,20 +49,22 @@ def main():
     #         log_dir='./log/{}'.format(dt.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")), write_images=True),
     # ]
 
-
-    # model = res_net_model()
-    ###ADD ARG strat_classes_num
-    # model.compile(optimizer=keras.optimizers.Adam(),
-    #               loss='LDAMLoss',
-    #               metrics=['TODO'])
-    ###KAROL: WE HAVE TO USE fit_generator()
+    model = res_net_model(strat_classes_num)
+    # ADD ARG strat_classes_num
+    cls_num_list = [1, 1, 1, 1, 1, 1, 1]
+    model.compile(optimizer=keras.optimizers.Adam(),
+                  loss=LDAMLoss(cls_num_list),
+                  metrics=['accuracy'])
+    # KAROL: WE HAVE TO USE fit_generator()
     # model.fit(train_dataset, epochs=30, steps_per_epoch=195,
     #           validation_data=valid_dataset,
     #           validation_steps=3, callbacks=callbacks)
+    model.fit(training_generator, validation_data=validation_generator)
 
 # further training from here
 
-#save data_test? use it for final assessment
+# save data_test? use it for final assessment
+
 
 if __name__ == '__main__':
     main()
