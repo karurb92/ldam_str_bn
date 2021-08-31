e3677ea95a8fdc4604a826e2dc68beb3e085a719
Nick Stracke <nick.stracke@web.de>
2021-08-31 11:35:51 +0200

    fix loss to work with multiple dims

--- losses.py
+++ losses.py
@@ -13,40 +13,36 @@ class LDAMLoss():
         assert s > 0
         self.s = s
         self.weight = weight
+        self.n_classes = len(cls_num_list)
 
     def __call__(self, x, target):
-        index = tf.zeros_like(x, dtype=tf.float32)
-        update = tf.constant([[1]],dtype=tf.float32)
-        # fix the problem
-        index = tf.reshape(index,(-1,1))
-        print("!!!!!!!!!!!!!!!!!!!!!!",index)
-        index_float = tf.tensor_scatter_nd_update(index, target, update)
-        batch_m = tf.matmul(self.m_list[None, :], index_float)
-        print("batch_m :",batch_m)
+        # this performs one hot encoding of target
+        # index = tf.zeros_like(x, dtype=tf.float32)
+        # update = tf.constant([[1]], dtype=tf.float32)
+        # index = tf.reshape(index, (-1, 1))
+        # print("!!!!!!!!!!!!!!!!!!!!!!", index)
+        # index_float = tf.tensor_scatter_nd_update(index, target, update)
+        index_float = tf.one_hot(target, self.n_classes)
+
+        batch_m = tf.matmul(self.m_list[None, :], tf.transpose(index_float))
+        print("batch_m :", batch_m)
         batch_m = tf.reshape(batch_m, (-1, 1))
-        print("batch_m :",batch_m)
+        print("batch_m :", batch_m)
         x_m = x - batch_m
-        print("x_m :",x_m)
+        print("x_m :", x_m)
 
         # if condition is true, return x_m[index], otherwise return x[index]
         index_bool = tf.cast(index_float, bool)
-        index_bool = tf.reshape(index_bool,(1,-1))
-        print("index :\n",index)
+        # index_bool = tf.reshape(index_bool, (1, -1))
         output = tf.where(index_bool, x_m, x)
-        print("output : ",output)
-        # Continue - For using tf softmax cross_entropy, we need labels and logits
-        labels = index_bool
-        logits = output
-        print("labels : \n",labels,"\n logits : \n", logits)
-        return tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=logits*self.s)
+        print("output : ", output)
 
-        '''
-        index.scatter_(1, target.data.view(-1, 1), 1)
-        index_float = index.type(torch.cuda.FloatTensor)
-        batch_m = torch.matmul(self.m_list[None, :], index_float.transpose(0,1))
-        batch_m = batch_m.view((-1, 1))
-        x_m = x - batch_m
-    
-        output = torch.where(index, x_m, x)
-        return F.cross_entropy(self.s*output, target, weight=self.weight)
-        '''
+        print(index_bool)
+        print(index_float)
+
+        labels = index_float
+        logits = output
+        print("labels : \n", labels, "\n logits : \n", logits)
+        loss = tf.nn.softmax_cross_entropy_with_logits(
+            labels=labels, logits=logits*self.s)
+        return tf.math.reduce_mean(loss)
--- test_ldam_loss.py
+++ test_ldam_loss.py
@@ -5,18 +5,22 @@ input space is R^d
 label space is {1,...,k}, y is corresponding to the label (Here k is 10)
 model f : R^d -> R^k and outputs k logits
 the inputs for Ldam loss is x,y 
-cls_num_list is [n1,n2,...,nk] -> the number of outputs for each classes
+cls_num_list is [n1,n2,...,nk] -> the number of instances of each classes
 '''
 #cls_num_list = [0,1,0,0,0,0,0,0,0,0]
-cls_num_list = [11,100,5,17,9,10,6,3,4,2]
+cls_num_list = [11, 100, 5, 17, 9, 10, 6, 3, 4, 2]
 ldam = losses.LDAMLoss(cls_num_list=cls_num_list)
 
-network_outputs = tf.constant([[.3, .7,0,0,0,0,0,0,0,0]])
-targets = tf.constant([[1]])
+network_outputs = tf.constant(
+    [[.3, .7, 0, 0, 0, 0, 0, 0, 0, 0], [.3, .7, 0, 0, 0, 0, 0, 0, 0, 0]])
+targets = tf.constant([1, 0])
+
+# network_outputs = tf.constant(
+#     [[.3, .7, 0, 0, 0, 0, 0, 0, 0, 0]])
+# targets = tf.constant([1])
 
 loss = ldam(network_outputs, targets)
-print("loss is",loss)
-
+print("loss is", loss)
 
 
 # import tensorflow.python.ops.numpy_ops.np_config
@@ -31,5 +35,3 @@ print("loss is",loss)
 
 # loss = ldam(network_outputs, targets)
 # print(loss)
-
-
