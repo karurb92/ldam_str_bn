302a4a7c642e92a4c78b4f55f6c210d101186037
Nick Stracke <nick.stracke@web.de>
2021-09-01 16:08:23 +0200

    cleanup

--- losses.py
+++ losses.py
@@ -16,35 +16,14 @@ class LDAMLoss():
         self.n_classes = len(cls_num_list)
 
     def __call__(self, target, x):
-        # this performs one hot encoding of target
-        # index = tf.zeros_like(x, dtype=tf.float32)
-        # update = tf.constant([[1]], dtype=tf.float32)
-        # index = tf.reshape(index, (-1, 1))
-        # print("!!!!!!!!!!!!!!!!!!!!!!", index)
-        # index_float = tf.tensor_scatter_nd_update(index, target, update)
-
-        # target = tf.cast(target, tf.uint8)
-        # index_float = tf.one_hot(target, self.n_classes)
         index_float = target
-
         batch_m = tf.matmul(self.m_list[None, :], tf.transpose(index_float))
-        #print("batch_m :", batch_m)
         batch_m = tf.reshape(batch_m, (-1, 1))
-        #print("batch_m :", batch_m)
-
-        # x = tf.cast(x, tf.float32)
-
         x_m = x - batch_m
-        #print("x_m :", x_m)
 
         # if condition is true, return x_m[index], otherwise return x[index]
         index_bool = tf.cast(index_float, bool)
-        # index_bool = tf.reshape(index_bool, (1, -1))
         output = tf.where(index_bool, x_m, x)
-        #print("output : ", output)
-
-        #print(index_bool)
-        #print(index_float)
 
         labels = index_float
         logits = output
